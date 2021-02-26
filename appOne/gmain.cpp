afee39c7cccec6484d331aad9d7ed18527d783fe
Coding-Ocean <eijo524@gmail.com>
2021-02-26 18:13:33 +0900

    recreate

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,88 +1,72 @@
 #include"libOne.h"
+struct VEC2 {
+    float x = 0, y = 0;
+};
 struct UNKO {
-   float px = 0, py = 0, vx = 0, vy = 0, ax = 0, ay=0, dx=0,dy=0;
+    VEC2 pos;
+    VEC2 vel;
+    VEC2 acc;
 };
 
 void gmain() {
-    window(1600,900);
-    int playerImg = loadImage("assets\\unkoPink.png");
-    int enemyImg = loadImage("assets\\unkoWhite.png");
-    struct UNKO p = { width/2,      height / 2 - 50 };
-    struct UNKO e = { width/2+200,  height / 2 - 50 };
+    window(1600, 300);
+    int unkoImg = loadImage("assets\\unkoWhite.png");
+    struct UNKO player = { width / 2,      height / 2 - 47 };
+    struct UNKO enemy = { width / 2 + 200,  height / 2 - 47 };
     float speed = 0;
     float maxSpeed = 10;
-    float force = 0.1;
-    float nearDist = 50;
-    float prePx;
+    float maxForce = 1.0f;
+    float nearDist = 100;
     rectMode(CENTER);
     while (notQuit) {
+        //テキスト表示位置ｙ
+        float py = 20;
+
+        clear(64,128,255);
+
         //プレイヤーの位置
         if (isTrigger(MOUSE_LBUTTON)) {
-            p.px = mouseX;
-            prePx = e.px;
+            player.pos.x = mouseX;
         }
+
         //enemyからplayerへのベクトル
-        e.dx = p.px - e.px;
+        float desire_x = player.pos.x - enemy.pos.x;
+        fill(255);
+        text("desire_x=" + (let)desire_x, 0, py); py += 20;
+
         //距離
-        float dist = absolute(e.dx);
-        //if (dist > 0.001f) {
-            //ベクトルの長さを1にする
-            if (dist != 0) {
-                e.dx /= dist;
-            }
-            //速度調節
-            if (dist > nearDist) {
-                //動き始め
-                if (speed < maxSpeed) {
-                    speed += force;
-                }
-                else {
-                    speed = maxSpeed;
-                }
-            }
-            else {
-                 //近づいたらスピードを落とす
-                 speed = maxSpeed * dist / nearDist;
-            }
-            //最終的なベクトル
-            e.vx = e.dx * speed;
-        //}
+        float dist = absolute(desire_x);
+        //スピードを決定。近くなったらスピードを落とす
+        speed = maxSpeed;
+        if (dist < nearDist) {
+             speed = maxSpeed * dist / nearDist;
+        }
+        //ベクトルの大きさをspeedにする
+        if (dist > 0) {
+            desire_x *= speed / dist;
+        }
+        //加速度
+        enemy.acc.x = desire_x - enemy.vel.x;
+        dist = absolute(enemy.acc.x);
+        if (dist > maxForce) {
+            enemy.acc.x *= maxForce / dist;
+        }
 
         //enemy移動
-        e.px += e.vx;
+        enemy.pos.x += enemy.vel.x;
+        enemy.vel.x += enemy.acc.x;
+        text("enemy.vel.x=" + (let)enemy.vel.x, 0, py); py += 20;
+        text("enemy.acc.x=" + (let)enemy.acc.x, 0, py); py += 20;
+        text("enemy.pos.x=" + (let)enemy.pos.x, 0, py); py += 20;
+        text("player.pos.x=" + (let)player.pos.x, 0, py); py += 20;
 
-        /*
-        //プレイヤーの位置
-        p.px = mouseX;
-        //enemyからplayerへのベクトル
-        e.vx = p.px - e.px;
-        //距離
-        float dist = e.vx > 0 ? e.vx : -e.vx;
-        //ベクトルの長さを１にする
-        if (dist != 0){
-            e.vx /= dist;
-        }
-        //近づいたらスピードを落とす
-        if (dist > nearDist){
-            speed = maxSpeed;
-        }
-        else{
-            speed = map(dist, 0, nearDist, 0, maxSpeed);
-        }
-        //最終的なベクトル
-        e.vx *= speed;
-        //enemy移動
-        e.px += e.vx;
-        */
-
-        clear(64, 128, 255);
-        line(0, height / 2, width, height / 2);
-        image(enemyImg, e.px, e.py);
-        image(playerImg, p.px, p.py);
-        text("e.vx=" + (let)e.vx, 0, 20);
-        text("e.dx=" + (let)e.dx, 0, 40);
-        text("dist=" + (let)dist, 0, 60);
-        text("speed=" + (let)speed, 0, 80);
+        noStroke();
+        fill(128, 64, 0);
+        rect(width/2, height / 4*3, width, height / 2);
+        imageColor(0, 128, 0);
+        image(unkoImg, enemy.pos.x, enemy.pos.y);
+        imageColor(255, 163, 229);
+        image(unkoImg, player.pos.x, player.pos.y);
 
     }
 }
